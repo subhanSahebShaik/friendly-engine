b868054c008979336c31ee63195975a6da36da83
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2025-01-27 16:38:07 +0530

    Update settings.py

--- transaction_manager/settings.py
+++ transaction_manager/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "https://cdn.jsdelivr.net/gh/subhanSahebShaik/friendly-engine@tree/main/static/"
+STATIC_URL = "https://cdn.jsdelivr.net/gh/subhanSahebShaik/friendly-engine@main/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
