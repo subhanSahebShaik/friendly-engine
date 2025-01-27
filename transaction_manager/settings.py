b713f180ec8092975ec4ea5bd92220d095edbe44
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2025-01-27 16:49:50 +0530

    Update settings.py

--- transaction_manager/settings.py
+++ transaction_manager/settings.py
@@ -110,12 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
+TIME_ZONE = 'Asia/Kolkata'
 USE_TZ = True
 
+USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
